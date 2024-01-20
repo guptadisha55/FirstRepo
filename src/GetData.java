c9d70ed9e5426cb762d1da662d4459ab36bf385f
guptadisha55 <157202461+guptadisha55@users.noreply.github.com>
2024-01-20 15:07:07 +0530

    Update GetData.java

--- src/GetData.java
+++ src/GetData.java
@@ -5,6 +5,7 @@ public class GetData {
 	public static void main(String[] args) throws Exception {
 		FetchFile file = new FetchFile();
 		file.readFileFrom();
+		FetchFile file2 = new FetchFile();
 	}
 
 }
